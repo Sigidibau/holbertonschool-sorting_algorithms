d50a99269e4ba5d61a9727a832b58f92e3f7069d
sigidibau <adriansantiago33@gail.com>
2024-08-07 12:36:01 -0400

    algorithm sor in a boubble style

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,10 +1,9 @@
 #include "sort.h"
 
 /**
- *
- *
- *
- *
+ *boubble_sort - function that sorts an array of integers in ascending order
+ *@array: pointer to and array
+ *@size: size of the array
  */
 
 void boubble_sort(int *array, size_t size)
@@ -13,6 +12,7 @@ void boubble_sort(int *array, size_t size)
 
 	for (i = 0; i < size; i++)
 	{
+		if (array[i] > array[i + 1])
 		array[i] = array[i + 1];
 		print_array(array, size);
 	}
