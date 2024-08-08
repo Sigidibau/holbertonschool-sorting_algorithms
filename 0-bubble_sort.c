a46716b87e34d7352ee2ae40c0ee9a8ee8461399
sigidibau <9571@holbertonstudents.com>
2024-08-07 23:42:26 -0400

    bubble sort function

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,15 +6,22 @@
  *@size: size of the array
  */
 
-void boubble_sort(int *array, size_t size)
+void bubble_sort(int *array, size_t size)
 {
+	int tmp = 0;
 	size_t i;
-
+	size_t a;
 	for (i = 0; i < size; i++)
 	{
-		if (array[i] > array[i + 1])
-		array[i] = array[i + 1];
-		print_array(array, size);
+		for (a = 0; a < size - 1; a++)
+		{
+		if (array[a] > array[a + 1])
+			{
+				tmp = array[a];
+				array[a] = array[a + 1];
+				array[a + 1] = tmp;
+				print_array(array, size);
+			}
+		}
 	}
-		return;
 }
